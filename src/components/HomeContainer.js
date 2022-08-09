ad6c5507efe0e6d35fd1f6b8c99ed6f18f4d6a51
AkashRoy5680 <royakash1272@gmail.com>
2022-08-09 05:54:12 +0200

    one item added in UI

--- src/components/HomeContainer.js
+++ src/components/HomeContainer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import bike from "./img/delivery.png"
 import HeroBg from "./img/heroBg.png"
+import I1 from "./img/i1.png"
 
 const HomeContainer = () => {
     return (
@@ -20,9 +21,18 @@ const HomeContainer = () => {
                 
                 <button type='button' className='bg-gradient-to-br from-blue-400 to-blue-500 w-full md:w-auto px-4 py-2 rounded-lg hover:shadow-lg transition-all ease-in-out duration-100'>order Now</button>
            </div>
-           <div className='py-2 flex-1 flex items-center'>
-                <img className='ml-auto h-420 w-full lg:w-auto lg:h-650' src={HeroBg} alt="" />
-                <div className='w-full h-full absolute flex items-center justify-center'></div>
+           <div className='py-2 flex-1 flex items-center relative'>
+                <img className='ml-auto h-420 w-full lg:w-auto lg:h-650 ' src={HeroBg} alt="" />
+                <div className='w-full h-full absolute top-0 left-0 flex items-center justify-center px-32 py-4'>
+                    <div className='w-190 p-4 bg-cardOverlay backdrop-blur-md rounded-3xl flex flex-col items-center justify-center '>
+                        <img className='w-40 -mt-20' src={I1} alt="" />
+                        <p className='text-lg font-semibold text-textColor mt-4'>IceCream</p>
+                        <p className='text-sm font-semibold text-lighttextGray my-3'>Chocolate & Vanilla</p>
+                        <p className="text-sm font-semibold text-headingColor">
+                  <span className="text-xs text-red-600">$</span>7.25
+                </p>
+                    </div>
+                </div>
            </div>
         </section>
     );
