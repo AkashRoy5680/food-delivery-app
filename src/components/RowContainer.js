1833cd9ac04283014de53bc30dfe80045ac4c001
AkashRoy5680 <royakash1272@gmail.com>
2022-08-11 00:58:16 +0200

    filter only fruits implemented

--- src/components/RowContainer.js
+++ src/components/RowContainer.js
@@ -1,23 +1,29 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { motion } from "framer-motion";
 import { MdShoppingBasket } from "react-icons/md";
-import bike from "./img/delivery.png"
 
-const RowContainer=({flag})=> {
+
+const RowContainer=({flag,data,scrollValue})=> {
+    console.log(data);
+    const rowContainer=useRef();
+  {/*  useEffect(() => {
+        rowContainer.current.scrollLeft += scrollValue;
+    }, [scrollValue]); */}
   return (
     <div className={`w-full flex items-center gap-3  my-12 scroll-smooth  ${
         flag
           ? "overflow-x-scroll scrollbar-none"
           : "overflow-x-hidden flex-wrap justify-center"
       }`}>
-        <div className='w-300 md:w-340 h-auto rounded-lg p-2 my-12 backdrop-blur-lg'>
+        {data && data.map(item => (
+            <div key={item.id} className='w-275 min-w-[300px] md:w-300 md:min-w-[300px] h-[175px] rounded-lg p-2 my-12 backdrop-blur-lg flex flex-col items-center justify-between'>
             <div className='w-full flex items-center justify-between'>
             <motion.div
                 className="w-40 h-40 -mt-8 drop-shadow-2xl"
                 whileHover={{ scale: 1.2 }}
               >
                 <img
-                  src={bike}
+                  src={item?.imageURL}
                   alt=""
                   className="w-full h-full object-contain"
                 />
@@ -31,15 +37,16 @@ const RowContainer=({flag})=> {
             </div>
 
             <div className='w-full flex flex-col items-end justify-end'>
-            <p className='text-textColor font-semibold text-base md:text-lg '>Chocolate & Vanilla</p>
-            <p className='mt-1 text-sm text-gray-500'>45 calories</p>
+            <p className='text-textColor font-semibold text-base md:text-lg '>{item?.title}</p>
+            <p className='mt-1 text-sm text-gray-500'>{item?.calories}</p>
             <div className='flex items-center gap-8 '>
                 <p className='text-lg text-headingColor font-semibold'>
-                    <span className='text-sm text-red-500'>$</span>5.25
+                    <span className='text-sm text-red-500'>$</span>{item?.price}
                 </p>
             </div>
             </div>
         </div>
+        ))}
       </div>
   )
 }
