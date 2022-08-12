44e0c54f8108f0df598518cab2fbc45016166a29
AkashRoy5680 <royakash1272@gmail.com>
2022-08-12 03:24:16 +0200

    cart section implemented

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -5,6 +5,7 @@ import {MdChevronLeft,MdChevronRight } from "react-icons/md";
 import { motion } from "framer-motion";
 import { useStateValue } from '../context/StateProvider';
 import MenuContainer from './MenuContainer';
+import CartContainer from './CartContainer';
 
 const MainContainer = () => {
   const [{foodItems},dispatch]=useStateValue();
@@ -49,6 +50,8 @@ const MainContainer = () => {
       </section>
 
       <MenuContainer/>
+
+      <CartContainer/>
         </div>
     );
 };
